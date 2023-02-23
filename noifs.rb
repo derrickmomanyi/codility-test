36e7a3e2f0cd860f9cf876a4b74f59b8345c19c9
Derrick Momanyi <derrick.momanyi@student.moringaschool.com>
2023-02-23 15:40:15 +0300

    Done

---
+++ noifs.rb
@@ -0,0 +1,18 @@
+def numbers(a,b)
+
+    case true
+        case true
+        when a>b
+             "#{a} is greater than #{b}"
+    
+        when a<b
+            "#{a} is smaller than #{b}"
+        
+        when a==b
+             "#{a} is equal to #{b}"
+        end
+
+
+end
+
+numbers(4,7)
